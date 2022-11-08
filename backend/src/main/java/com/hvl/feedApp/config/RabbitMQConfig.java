a3c5f866be2776b7e1f18800fd2f9403f4d02462
trenod <torewb@gmail.com>
2022-11-08 15:33:35 +0100

    connectionFactory is null, trying to fix

--- backend/src/main/java/com/hvl/feedApp/config/RabbitMQConfig.java
+++ backend/src/main/java/com/hvl/feedApp/config/RabbitMQConfig.java
@@ -1,6 +1,7 @@
 package com.hvl.feedApp.config;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -15,11 +16,20 @@ public class RabbitMQConfig {
     public static final String POLL_FINISH = "q.poll-finish";
 
 
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost");
+        connectionFactory.setUsername("guest");
+        connectionFactory.setPassword("guest");
+        return connectionFactory;
+    }
+
     /*
     @Bean
     public CachingConnectionFactory connectionFactory() {
         return new CachingConnectionFactory("localhost");
     }
+
 
     @Bean
     public RabbitAdmin amqpAdmin() {
@@ -30,14 +40,14 @@ public class RabbitMQConfig {
     @Bean
     public RabbitTemplate jsonRabbitTemplate(ConnectionFactory connectionFactory) {
         RabbitTemplate template = new RabbitTemplate(connectionFactory);
-        //template.setMessageConverter(jsonConverter());
+        template.setMessageConverter(jsonConverter());
         return template;
     }
 
-    //@Bean
-    //public MessageConverter jsonConverter() {
-        //return new Jackson2JsonMessageConverter();
-    //}
+    @Bean
+    public MessageConverter jsonConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
 
     @Bean
     public Queue createPollQueue() {
