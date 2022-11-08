f765bb88de68e53125ea77a69b03e9318c26402e
Ingvild Stensland <56112778+wuw012@users.noreply.github.com>
2022-11-08 12:46:14 +0100

    removed auth0

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -13,31 +13,5 @@
   </div>
 </template>
 <script lang="ts">
-// Composition API
-import { useAuth0 } from '@auth0/auth0-vue';
-
-
-export default {
-  setup() {
-    const auth0 = useAuth0();
-    return {
-      login() {
-        auth0.loginWithRedirect();
-        
-      },
-      logout() {
-          auth0.logout({ returnTo: window.location.origin });
-        },
-      user: auth0.user,
-      isAuthenticated: auth0.isAuthenticated, // Always check before consuming user
-      isLoading: auth0.isLoading, // Always check before accessing isAuthenticated
-    };
-
-  }
-/* 
-  created() {
-    let loginStatus = this.$cookies.get();
-  } */
-};
 
 </script>
