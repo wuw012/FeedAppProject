158a1dd99c7f12c29a55d392eddec4949bf44ce7
trenod <torewb@gmail.com>
2022-11-09 17:56:18 +0100

    Imported files to get inspiration from.

---
+++ backend/src/main/java/com/hvl/feedApp/messagingsio/Receiver.java
@@ -0,0 +1,22 @@
+package com.hvl.feedApp.messagingsio;
+
+import java.util.concurrent.CountDownLatch;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Receiver {
+
+	private CountDownLatch latch = new CountDownLatch(1);
+
+	public void receiveMessage(String message) {
+		System.out.println("Received <" + message + ">");
+		latch.countDown();
+	}
+
+	public CountDownLatch getLatch() {
+		return latch;
+	}
+
+}
