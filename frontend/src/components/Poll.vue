dc2623a19cc7a1d4f8a30d3e5906c27599c1a716
Ingvild Stensland <56112778+wuw012@users.noreply.github.com>
2022-11-12 22:23:23 +0100

    getPoll and postVote logic in poll component

--- frontend/src/components/Poll.vue
+++ frontend/src/components/Poll.vue
@@ -2,63 +2,85 @@
    <div class="poll">
    <h3> ID: {{ $route.params.pollID }} </h3>
     <div class="question">
-      <h1> Q: {{ questionText }} </h1>
+      <h1> Q: {{ this.poll.question }} </h1>
     </div>
 
     <div class="buttongroup">
-      <button class="btn btn-primary btn-lg mr-2" type="submit" v-on:click="redirectToThankyou()"> YES </button>
-      
-      <button class="btn btn-primary btn-lg" type="submit" v-on:click="redirectToThankyou()">NO</button>
+      <button class="btn btn-primary btn-lg mr-2" type="submit" v-on:click="voteYes()"> YES </button>
+      <button class="btn btn-primary btn-lg" type="submit" v-on:click="voteNo()">NO</button>
     </div>
+
    </div>
 </template>
   
 
 <script lang="ts">
 import { RouterLink, RouterView } from "vue-router";
+import FeedAppDataService from "@/services/FeedAppDataService";
 
 export default {
   name: 'Poll',
-  props: {
-    questionText: {
-      type: String,
-    },
-    pincode: {
-      type: Number,
-    },
-    counter: {
-      type: Number,
-    },
+  data() {
+    return {
+      poll: {},
+      hasVoted: false,
+    }
   },
   methods: {
+    // HELPER METHODS
     redirectToThankyou() {
       this.$router.push({path: "/thankyou"})
+    },
+    retrieveUserCredentialsFromLocalStorage(){
+        this.username = localStorage.getItem("username")
+        this.password = localStorage.getItem("password")
+    },
+    // GET POLL LOGIC
+    async getPoll() {
+      this.retrieveUserCredentialsFromLocalStorage();
+      await FeedAppDataService.getPoll(this.$route.params.pollID, this.username, this.password)
+      .then((poll) => this.poll = poll);
+    },
+    retrievePoll() {
+      this.getPoll().then()
+    },
+    // POST VOTE LOGIC
+    async postVote( answer_yes : bool ) {
+      this.retrieveUserCredentialsFromLocalStorage();
+      await FeedAppDataService.postVote(answer_yes, this.$route.params.pollID, this.username, this.password)
+      .then((status) => {
+        if (status == 200) {
+          this.hasVoted = true;
+        }
+      })
+    },
+    voteNo() {
+      if (!this.hasVoted) {
+        this.postVote(false).then(() => {
+          if (this.hasVoted) {
+            this.redirectToThankyou()
+          }
+        })
       }
+    },
+    voteYes() {
+      this.postVote(true).then(() => {
+        if (this.hasVoted) {
+            this.redirectToThankyou()
+          }
+        })
+    },
+    
+  },
+  mounted() {
+    this.retrievePoll()
   }
 }
 
 </script>
 
 <style scoped>
-a{
-  color: #fff;
-}
-a:hover{
-  background-color: #007bff;
-}
-
 h3{
   text-align:right;
 }
-
-.question{
-  display: block;
-}
-.poll{
-      text-align: center;
-    }
-.button{
-      text-align: center;
-      margin: 200em;
-    }
 </style>
