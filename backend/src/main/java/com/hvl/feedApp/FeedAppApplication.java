ac5d40b694616767ee52995ec97025d083f908a6
thomassi <669285@stud.hvl.no>
2022-11-14 17:59:07 +0100

    Implement scheduled Poll update posts to Dweet.io

--- backend/src/main/java/com/hvl/feedApp/FeedAppApplication.java
+++ backend/src/main/java/com/hvl/feedApp/FeedAppApplication.java
@@ -1,12 +1,15 @@
 package com.hvl.feedApp;
 
+import com.hvl.feedApp.taskScheduler.ScheduledTasks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableScheduling
 public class FeedAppApplication {
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
@@ -20,5 +23,7 @@ public class FeedAppApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(FeedAppApplication.class, args);
+		//SpringApplication.run(ScheduledTasks.class, args);
+
 	}
 }
